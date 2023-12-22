5fe3be3e19d4d6502763b1af47b13cd86e5b7386
bigetti <upgrade81@mail.ru>
2023-12-22 15:23:25 +0300

    Trying paginator

--- pagination/pagination/settings.py
+++ pagination/pagination/settings.py
@@ -126,7 +126,10 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'assets'),
 ]
 
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BUS_STATION_CSV = os.path.join(BASE_DIR, 'data-398-2018-08-30.csv')
+# BUS_STATION_CSV = os.path.join(BASE_DIR, 'data-398-2018-08-30.csv')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- pagination/stations/views.py
+++ pagination/stations/views.py
@@ -1,5 +1,10 @@
+import csv
+
+from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
 from django.urls import reverse
+from django.conf import settings
+
 
 
 def index(request):
@@ -9,9 +14,28 @@ def index(request):
 def bus_stations(request):
     # получите текущую страницу и передайте ее в контекст
     # также передайте в контекст список станций на странице
+    # Путь к csv-файлу
+    csv_file_path = settings.BUS_STATION_CSV
 
+    # Чтение данных из csv-файла
+    with open(csv_file_path, encoding='utf-8') as csvfile:
+        reader = csv.DictReader(csvfile)
+        stations_data = list(reader)
+
+        # Количество элементов на странице
+    items_per_page = 10
+
+    # Создание объекта Paginator
+    paginator = Paginator(stations_data, items_per_page)
+
+    # Получение текущей страницы из GET-параметра
+    page_number = request.GET.get('page', 1)
+
+    # Получение объекта Page для текущей страницы
+    page_obj = paginator.get_page(page_number)
+
+    # Формирование контекста
     context = {
-    #     'bus_stations': ...,
-    #     'page': ...,
+        'bus_stations': page_obj,
     }
     return render(request, 'stations/index.html', context)
